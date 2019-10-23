a19b2cf9061c818b29374048aa60e313213ac771
alejandromb <alejandrobrito88@gmail.com>
2019-10-22 20:40:07 -0400

    adding function end2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+
 # miscellaneous_prog
+67 45 32 01 little endian
+02 23 45 67 big endian
--- endiannes.cpp
+++ endiannes.cpp
@@ -1,10 +1,26 @@
-#include<stdio.h>
+
+include<stdio.h>
+void endian()  
+{  
+    unsigned int i = 1;  
+    char *c = (char*)&i;  
+    if (*c)  
+        cout<<"Little endian";  
+    else
+        cout<<"Big endian"; 
+}  
+
+
+
+
+
 void show_mem_rep(char *start, int n)  
 { 
     int i; 
     for (i = 0; i < n; i++) 
          printf(" %.2x", start[i]); 
     printf("\n"); 
+ endian();
 } 
   
 /*Main function to call above function for 0x01234567*/
